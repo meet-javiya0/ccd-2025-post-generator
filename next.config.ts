10cc81def5688d3ea6b7070f836cee1e93cd7a88
meetkumar.javiya <meetkumar.javiya@improwised.com>
2025-06-21 15:30:14 +0530

    modified next config file

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,23 @@
-import type {NextConfig} from 'next';
-
-const isProd = process.env.NODE_ENV === 'production'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? '/ccd-2025-post-generator' : '',
-  basePath: isProd ? '/ccd-2025-post-generator' : '',
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
